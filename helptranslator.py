b1a6d26fb2c962af7c2f8611769658e9dcb885e2
Nicholas A. Del Grosso <delgrosso.nick@gmail.com>
2018-04-23 01:21:58 +0200

    added some module parser functions for future offline use, although they don't currently work yet

--- helptranslator.py
+++ helptranslator.py
@@ -1,6 +1,8 @@
 import sys
 import pydoc
+import itertools
 import googletrans
+import json
 
 _original_output = pydoc.help._output
 
@@ -25,3 +27,34 @@ def set_help_lang(language):
 
     pydoc.help._output = STDOutTrans()
     return
+
+
+def parse_module_doc(module):
+    """Returns a dict containing all the help text in a module."""
+    docs = {'module': module.__name__}
+    for fun in itertools.chain([module], module.__dict__):
+        if hasattr(fun, '__doc__'):
+            docs[str(fun)] = fun.__doc__
+    return docs
+
+
+def translate_langdict(langdict, lang):
+    """calls Google translate on all values in a dict, returning a translated dict."""
+    translated_dict = {'module': langdict['module']}
+    for fun, string in langdict.items():
+        translator = googletrans.Translator()
+        translated = translator.translate(string, dest=lang)
+        translated_dict[fun] = translated.text
+    return translated_dict
+
+
+def write_lang_stubfile(langdict):
+    """Writes the translations from a langdict to file."""
+    with open(langdict['module'] + '.doclang', 'w') as f:
+        json.dump(langdict, f)
+
+
+def read_lang_stubfile(fname):
+    """Returns a dict from a '.doclang' file name"""
+    with open(fname) as f:
+        return json.load(f)
